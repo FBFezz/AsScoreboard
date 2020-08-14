1bbfa284a8937f1837a073359fe1aa8e93ae9449
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-08-14 11:20:41 +0100

    Update to fxmanifest and cleaned up

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game  'gta5'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/style.css',
+	'html/index.js'
+}
